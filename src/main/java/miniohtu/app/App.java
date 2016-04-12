26e581d58d9267ab2cf7e5587335d6fd89eedfb1
Vililassi Ala-Tuuhonen <vililassi.ala-tuuhonen@traphound.com>
2016-04-12 21:05:26 +0300

    Initial Maven project

---
+++ src/main/java/miniohtu/app/App.java
@@ -0,0 +1,13 @@
+package miniohtu.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
