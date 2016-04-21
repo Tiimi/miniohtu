bfda7b38d80bd5d23850257d12b38d963e7a4adc
Antti Haapaniemi <antti.haapaniemi@helsinki.fi>
2016-04-21 17:46:36 +0300

    Viite -> bibtex valmis ja testit.

--- src/main/java/miniohtu/bibtex/ViiteBibtex.java
+++ src/main/java/miniohtu/bibtex/ViiteBibtex.java
@@ -2,25 +2,52 @@ package miniohtu.bibtex;
 
 import miniohtu.entry.Article;
 import miniohtu.entry.Book;
+import miniohtu.entry.Booklet;
 
 public class ViiteBibtex {
 
-    public static String toBibtex(Article a) {
-        String bibtex = "@ARTICLE{" + a.getId() + "},\n"
-                + "  author = {" + a.getAuthor() + "},\n"
-                + "  title = {" + a.getTitle() + "},\n"
-                + "  yournal = {" + a.getJournal() + "},\n"
-                + "  year = {" + a.getYear() + "},\n"
-                + ((a.getNumber() == Integer.MAX_VALUE) ? "" : "  number = " + a.getNumber() + ",\n")
-                + ((a.getPages() == null) ? "" : "  pages = {" + a.getPages() + "},\n")
-                + ((a.getMonth() == Integer.MAX_VALUE) ? "" : "  month = " + a.getMonth() + ",\n")
-                + ((a.getNote() == null) ? "" : "  note = {" + a.getNote() + "},\n")
-                + ((a.getVolume()  == Integer.MAX_VALUE) ? "" : " volume = " + a.getVolume() + ",\n")
+    public static String toBibtex(Article article) {
+        String bibtex = "@ARTICLE{" + article.getId() + "},\n"
+                + "  author = {" + article.getAuthor() + "},\n"
+                + "  title = {" + article.getTitle() + "},\n"
+                + "  yournal = {" + article.getJournal() + "},\n"
+                + "  year = {" + article.getYear() + "},\n"
+                + ((article.getNumber() == Integer.MAX_VALUE) ? "" : "  number = " + article.getNumber() + ",\n")
+                + ((article.getPages() == null) ? "" : "  pages = {" + article.getPages() + "},\n")
+                + ((article.getMonth() == null) ? "" : "  month = " + article.getMonth() + ",\n")
+                + ((article.getNote() == null) ? "" : "  note = {" + article.getNote() + "},\n")
+                + "  volume = " + article.getVolume() + ",\n"
                 + "}";
         return bibtex;
     }
+
+    public static String toBibtex(Book book) {
+        String bibtex = "@ARTICLE{" + book.getId() + "},\n"
+                + "  author = {" + book.getAuthor() + "},\n"
+                + "  title = {" + book.getTitle() + "},\n"
+                + "  publisher = {" + book.getPublisher() + "},\n"
+                + "  year = {" + book.getYear() + "},\n"
+                + ((book.getVolume() == Integer.MAX_VALUE) ? "" : "  volume = " + book.getVolume() + ",\n")
+                + ((book.getSeries() == null) ? "" : "  series = {" + book.getSeries() + "},\n")
+                + ((book.getAddress() == null) ? "" : "  address = {" + book.getAddress() + "},\n")
+                + ((book.getEdition() == Integer.MAX_VALUE) ? "" : "  edition = " + book.getEdition() + ",\n")
+                + ((book.getMonth() == null) ? "" : "  month = {" + book.getMonth() + "},\n")
+                + ((book.getNote() == null) ? "" : "  note = {" + book.getNote() + "},\n")
+                + "}";
+
+        return bibtex;
+    }
     
-    public static String toBibtex(Book b) {
-        return null;
+    public static String toBibtex(Booklet booklet) {
+        String bibtex = "@BOOKLET{" + booklet.getId() + "},\n"
+                + "  title = {" + booklet.getTitle() + "},\n"
+                + ((booklet.getAuthor()== null) ? "" : "  author = {" + booklet.getAuthor() + "},\n")            
+                + ((booklet.getHowPublished()== null) ? "" : "  howpublished = {" + booklet.getHowPublished()+ "},\n")
+                + ((booklet.getAddress() == null) ? "" : "  address = {" + booklet.getAddress() + "},\n")
+                + ((booklet.getMonth() == null) ? "" : "  month = {" + booklet.getMonth() + "},\n")
+                + ((booklet.getYear() == Integer.MAX_VALUE) ? "" : "  year = {" + booklet.getYear() + "},\n")
+                + ((booklet.getNote() == null) ? "" : "  note = {" + booklet.getNote() + "},\n")
+                + "}";
+        return bibtex;
     }
 }
--- src/test/java/miniohtu/bibtex/ViiteBibtexTest.java
+++ src/test/java/miniohtu/bibtex/ViiteBibtexTest.java
@@ -1,6 +1,8 @@
 package miniohtu.bibtex;
 
 import miniohtu.entry.Article;
+import miniohtu.entry.Book;
+import miniohtu.entry.Booklet;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -31,20 +33,21 @@ public class ViiteBibtexTest {
 
     @Test
     public void mandatoryFieldArticleToBibTest() {
-        Article article = new Article("article", "Petteri Petterinen", "The Title", "The Journal", 2000);
+        Article article = new Article("article", "Petteri Petterinen", "The Title", "The Journal", 2000, 1);
         String expected = "@ARTICLE{article},\n"
                 + "  author = {Petteri Petterinen},\n"
                 + "  title = {The Title},\n"
                 + "  yournal = {The Journal},\n"
                 + "  year = {2000},\n"
+                + "  volume = 1,\n"
                 + "}";
         assertEquals(expected, ViiteBibtex.toBibtex(article));
     }
-    
+
     @Test
     public void allFieldsArticleToBibTest() {
-                Article article = new Article("article", "Petteri Petterinen", "The Title", "The Journal", 2000, 1,1,"1-2", 2,"This is a note");
-                String expected = "@ARTICLE{article},\n"
+        Article article = new Article("article", "Petteri Petterinen", "The Title", "The Journal", 2000, 1, 1, "1-2", "2", "This is a note");
+        String expected = "@ARTICLE{article},\n"
                 + "  author = {Petteri Petterinen},\n"
                 + "  title = {The Title},\n"
                 + "  yournal = {The Journal},\n"
@@ -54,7 +57,74 @@ public class ViiteBibtexTest {
                 + "  month = 2,\n"
                 + "  note = {This is a note},\n"
                 + "  volume = 1,\n"
-                + "}";             
-                assertEquals(expected, ViiteBibtex.toBibtex(article));
+                + "}";
+        assertEquals(expected, ViiteBibtex.toBibtex(article));
+    }
+
+    @Test
+    public void mandatoryFieldsBookToBibtest() {
+        Book book = new Book("book", "Petteri Petterson", "The Title", "The Publisher", 2000);
+        String expected = "@ARTICLE{book},\n"
+                + "  author = {Petteri Petterson},\n"
+                + "  title = {The Title},\n"
+                + "  publisher = {The Publisher},\n"
+                + "  year = {2000},\n"
+                + "}";
+        assertEquals(expected, ViiteBibtex.toBibtex(book));
+    }
+
+    @Test
+    public void allFieldsBookToBibTest() {
+        Book book = new Book("book", "Petteri Petterson", "The Title", "The Publisher", 2000, 1, "series", "address", 0, "Month", "This is a note", null);
+        String expected = "@ARTICLE{book},\n"
+                + "  author = {Petteri Petterson},\n"
+                + "  title = {The Title},\n"
+                + "  publisher = {The Publisher},\n"
+                + "  year = {2000},\n"
+                + "  volume = 1,\n"
+                + "  series = {series},\n"
+                + "  address = {address},\n"
+                + "  edition = 0,\n"
+                + "  month = {Month},\n"
+                + "  note = {This is a note},\n"
+                + "}";
+        assertEquals(expected, ViiteBibtex.toBibtex(book));
+    }
+
+    @Test
+    public void allFieldsBookletToBibTest() {
+        Booklet booklet = new Booklet("book", "The title", "Author", "Somehow", "Address", "Month", 2000, "This is a note.", null);
+        String expected = "@BOOKLET{book},\n"
+                + "  title = {The title},\n"
+                + "  author = {Author},\n"
+                + "  howpublished = {Somehow},\n"
+                + "  address = {Address},\n"
+                + "  month = {Month},\n"
+                + "  year = {2000},\n"
+                + "  note = {This is a note.},\n"
+                + "}";
+        assertEquals(expected, ViiteBibtex.toBibtex(booklet));
+    }
+    
+    @Test
+    public void someFieldsMissingBookletToBibTest() {
+        Booklet booklet = new Booklet("book", "The title", "Author", "Somehow", null, "Month", 2000, null, null);
+        String expected = "@BOOKLET{book},\n"
+                + "  title = {The title},\n"
+                + "  author = {Author},\n"
+                + "  howpublished = {Somehow},\n"
+                + "  month = {Month},\n"
+                + "  year = {2000},\n"
+                + "}";
+        assertEquals(expected, ViiteBibtex.toBibtex(booklet));
+    }
+
+    @Test
+    public void mandatoryFieldsBookletToBibTest() {
+        Booklet booklet = new Booklet("book", "The title");
+        String expected = "@BOOKLET{book},\n"
+                + "  title = {The title},\n"
+                + "}";
+        assertEquals(expected, ViiteBibtex.toBibtex(booklet));
     }
 }
